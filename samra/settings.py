fdf355bccff70419aafad555061e2543bec582e4
Tristan Downing <downing.tristan@gmail.com>
2022-08-15 17:56:27 +0000

    updated requirements

--- samra/settings.py
+++ samra/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '0.0.0.0',
+]
 
 
 # Application definition
