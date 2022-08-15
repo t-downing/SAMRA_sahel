be0557ff4df3859b8468f16d9f08c99fc51e802f
Tristan Downing <downing.tristan@gmail.com>
2022-08-15 19:46:32 +0000

    added settings for psql

--- samra/settings.py
+++ samra/settings.py
@@ -85,7 +85,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql_psycopg2",
+    #     "NAME": "d446lmk1hkk665",
+    #     "USER": os.environ.get("PSQL_USER"),
+    #     "PASSWORD": os.environ.get("PSQL_PASSWORD"),
+    #     "HOST": "ec2-18-204-142-254.compute-1.amazonaws.com",
+    #     "PORT": "5432",
+    # }
 }
 
 
