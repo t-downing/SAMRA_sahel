5e66452f694a691193d3868271c8c94e7959735e
Tristan Downing <downing.tristan@gmail.com>
2022-08-16 21:47:59 +0000

    rerouted home link

--- sahel/urls.py
+++ sahel/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
+    # path("home", views.IndexView.as_view(), name="index"),
     path("model_diagram", views.ModelDiagramView.as_view(), name="model_diagram"),
     path("django_plotly_dash/", include("django_plotly_dash.urls")),
     path("equation_bank", views.EquationBankView.as_view(), name="equation_bank"),
     path("comparison", views.ComparisonView.as_view(), name="comparison"),
     path("scenarios", views.ScenarioView.as_view(), name="scenarios"),
     path("sources", views.SourceListView.as_view(), name="sources"),
-    path("scenarioresponse", views.ScenarioResponseView.as_view(), name="scenarioresponse")
+    path("", views.ScenarioResponseView.as_view(), name="scenarioresponse")
 ]
