f0ff1430d98c72ea7c5953f79b33ceef36d63f06
A_Tristan DOWNING <a_tdowning@icrc.org>
2022-11-18 16:51:44 +0100

    data_mgmt forecast

--- sahel/management/commands/data_mgmt.py
+++ sahel/management/commands/data_mgmt.py
@@ -1,11 +1,15 @@
 from django.core.management.base import BaseCommand
 from sahel.models import *
 from datetime import date
+from sahel.sd_model.forecasting import forecast_element
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        Element.objects.filter(samramodel_id__isnull=True).update(samramodel_id=2)
+        elements = Variable.objects.filter(sd_type="Input").exclude(measureddatapoints=None)
+        for element in elements:
+            print(element)
+            forecast_element(element.pk)
         pass
 
 
