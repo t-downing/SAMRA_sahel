56daa7b1cad815d44476cc999cb77572e6353b79
Tristan Downing <downing.tristan@gmail.com>
2023-03-09 10:32:19 +0000

    swapped to new schema

--- sahel/urls.py
+++ sahel/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path("model_diagram", views.ModelDiagramView.as_view(), name="model_diagram"),
+    path("model_diagram", views.Mapping2Modeling.as_view(), name="model_diagram"),
     path("django_plotly_dash/", include("django_plotly_dash.urls")),
     path("equation_bank", views.EquationBankView.as_view(), name="equation_bank"),
     path("responses", views.ResponseView.as_view(), name="responses"),
@@ -14,6 +14,6 @@ urlpatterns = [
     path("scenarioresponse", views.ScenarioResponseView.as_view(), name="scenarioresponse"),
     path("termsoftrade", views.TermsOfTradeView.as_view(), name="termsoftrade"),
     path("response_list", views.ResponseListView.as_view(), name="response_list"),
-    path("mapping2modeling", views.Mapping2Modeling.as_view(), name="mapping2modeling"),
+    path("archive_schema", views.ModelDiagramView.as_view(), name="mapping2modeling"),
     path("evidencebit/create", views.EBCreateView.as_view(), name="eb_create"),
 ]
